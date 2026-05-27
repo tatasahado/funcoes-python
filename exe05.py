cc640522c8494e69df3476e778ef72b3eca92998
ADS 2026.1N <36140532026.1N@senacrjedu.local>
2026-05-27 20:11:23 -0300

    Add exe05

---
+++ exe05.py
@@ -0,0 +1,15 @@
+def calculadora(a, b, operacao):
+    if operacao == "+":
+        result = a + b
+    elif operacao == "-":
+        result = a - b
+    elif operacao == "*":
+        result = a * b
+    else:
+        result = a / b
+    return result
+
+print(calculadora(4, 2, "+"))
+print(calculadora(4, 2, "-"))
+print(calculadora(4, 2, "*"))
+print(calculadora(4, 2, "/"))
