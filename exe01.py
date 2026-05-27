2889278f76fa1b83f4197c2fea9ff64650665873
ADS 2026.1N <36140532026.1N@senacrjedu.local>
2026-05-27 19:35:50 -0300

    add exe02

---
+++ exe01.py
@@ -0,0 +1,6 @@
+def saudacao(nome):
+    print (f"Olá, {nome}")
+    
+
+saudacao("Bruno")
+saudacao("Taissa")
---
+++ exe02.py
@@ -0,0 +1,10 @@
+def somar (a,b):
+    return a + b
+
+result = somar(2, 7)
+result2 = somar(5, 3)
+    
+print(result)
+print(result2)
+
+print(somar())
